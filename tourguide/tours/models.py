6d953e270ce819f2ad338cef71890ab99ee4bfe5
Sholom Schonbuch <sholomschonbuch@gmail.com>
2024-08-28 17:17:02 -0400

    Added the Tour model

--- tourguide/tours/models.py
+++ tourguide/tours/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 class Tour(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    duration = models.IntegerField()
-    max_participants = models.IntegerField()
-    date = models.DateField()
-    time = models.TimeField()
-    meeting_point = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='tour_images')
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    duration = models.IntegerField(null=True)
+    max_participants = models.IntegerField(null=True)
+    date = models.DateField(null=True)
+    time = models.TimeField(null=True)
+    #meeting_point = models.CharField(max_length=100)
+    #image = models.ImageField(upload_to='tour_images')
 
     def __str__(self):
         return self.title
--- tourguide/tours/views.py
+++ tourguide/tours/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def tours(request):
+    return HttpResponse("Hello World")
